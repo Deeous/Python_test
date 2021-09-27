976c663824920f94b3d0f33391c207e22af23856
Karim Abushaev <karim_abushaev@mail.ru>
2021-09-27 21:56:28 +0300

    вторая версия

--- main.py
+++ main.py
@@ -2,11 +2,12 @@ def get_vat(payment, percent=13):
     print(percent)
     try:
         payment = float(payment)
+        print(payment)
         percent = int(percent)
         vat = payment / 100 * percent
         vat = round(vat,2)
         return 'Сумма НДФЛ: {}'.format(vat)
     except (TypeError, ValueError):
         print("не могу посчитать, проверьте вводимые данные")
-result = get_vat(400, 15)
+result = get_vat(800, 15)
 print(result)
